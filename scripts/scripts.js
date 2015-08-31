214eeee00e1eeaabec236543cc7b52fbe8d9b914
Sarah Klein <Ssk13@ufl.du>
2015-08-31 18:13:09 -0400

    added new features to photo gallery
    Captions now render below pictures, images can be browsed with
    navigational arrows and keyboard arrow keys, lightboxes are dismissed
    with esc key

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,16 +10,67 @@ $(document).ready(function(){
 	});
 
 	$('.x-button').on('click', function() {
+		closeStuff();
+	});
+
+	function closeStuff () {
 		if (!($('#Contact').hasClass('hide'))) {
 			$('#Contact').addClass('hide');
 		}
 		if (!($('#imageContainer').hasClass('hide'))) {
 			$('#imageContainer').addClass('hide');
 		}
-	});
+	}
 
 	$('.expandable-img').on('click', function() {
 		$('#image').css('background-image', 'url("' + $(this).attr('src').replace('thumb', 'img') + '")');
+		$('#image').data('value', $(this).data('value'));
+		$('#Caption').html($(this).data('caption'));
 		$('#imageContainer').removeClass('hide');
 	})
+
+	$('.imgArrowBox').on('click', function() {
+		navigateImages($(this).hasClass('left'));
+	});
+
+	function navigateImages(isLeftArrow) {
+		if ($('#imageArrowBox').hasClass('hide'))
+			return;
+
+		var images,
+			currentValue = $('#image').data('value'),
+			newValue;
+
+		images = $('#Hobbies').find('.expandable-img');
+
+		if (isLeftArrow) {
+			newValue = (currentValue - 1 < 1) ? images.length : currentValue - 1;
+		} else {
+			newValue = (currentValue + 1 > images.length) ? 1 : currentValue + 1;
+		}
+
+		$('#image').css('background-image', 'url("' + $(images[newValue - 1]).attr('src') + '")');
+		$('#Caption').html($(images[newValue - 1]).data('caption'));
+		$('#image').data('value', $(images[newValue - 1]).data('value'));
+	}
+
+	document.onkeydown = function(event) {
+        if (!event)
+            event = window.event;
+        var code = event.keyCode;
+        if (event.charCode && code == 0)
+            code = event.charCode;
+        switch(code) {
+            case 27: 		//esc
+                closeStuff();
+                break;
+            case 37: 		//left arrow
+                navigateImages(true);
+                break;
+            case 39: 		//right arrow
+                navigateImages(false);
+                break;
+        }
+        event.preventDefault();
+    };
 });
